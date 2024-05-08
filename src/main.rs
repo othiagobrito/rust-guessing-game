8518f7c923d1f452ab5d233102c11d9bac598741
Thiago <thiago7brito@gmail.com>
2024-05-08 09:37:08 -0300

    loop added

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,29 @@ use rand::Rng;
 fn main() {
     let random_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Try to guess the number between 1 and 100:");
-
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("You should type a number!");
-
-    println!("Your guess was: {guess}");
-
-    match guess.cmp(&random_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Winner Winner Chicken Dinner"),
+    loop {
+        println!("Try to guess the number between 1 and 100:");
+    
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You should type a number!");
+                continue;
+            },
+        };
+    
+        println!("Your guess was: {guess}");
+    
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Winner Winner Chicken Dinner");
+                break;
+            },
+        }
     }
 
     println!("The number was: {random_number}");
